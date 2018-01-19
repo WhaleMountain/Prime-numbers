f9cff6544c5e0fec13068c353364fff0a83d77fd
Ken Miyahira <e175733@ie.u-ryukyu.ac.jp>
2018-01-19 13:17:24 +0900

    メモリを食べる問題を解決

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -9,9 +9,25 @@ public class PrimeNumbers{
         int sur = 0;
         int s_cnt = 0;
         int r_cnt = 0;
-        int p_cnt = 0;
-        int[] result = new int[num];
 
+        for(int i=2;i<=num;i++){
+            for(int j=1;j<=i;j++){
+                sur=i%j;
+                if(sur==0){
+                    s_cnt++;
+                }
+                if(s_cnt==3){
+                    break;
+                }
+            }
+            if(s_cnt==2){
+                r_cnt++;
+            }
+            s_cnt=0;
+        }
+
+        int[] result = new int[r_cnt];
+        r_cnt=0;
         for(int i=2;i<=num;i++){
             for(int j=1;j<=i;j++){
                 sur=i%j;
@@ -29,19 +45,13 @@ public class PrimeNumbers{
             s_cnt=0;
         }
 
-        for(int i=0;i<result.length;i++){
-            if(result[i]!=0){
-                p_cnt++;
-            }
-        }
-
         System.out.println();
-        System.out.println(num+"までの素数の数は"+p_cnt+"つです。");
+        System.out.println(num+"までの素数の数は"+r_cnt+"つです。");
         System.out.println("一覧を表示しますか？");
         System.out.print("please Yes or No ->");
         String judge = s_num.nextLine();
         if(judge.equals("Yes")||judge.equals("yes")||judge.equals("YES")||judge.equals("y")){
-            for(int i=0;i<p_cnt;i++){
+            for(int i=0;i<r_cnt;i++){
                 System.out.println((i+1)+"つ目の素数:"+result[i]);
             }
         }else{
